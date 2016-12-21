dcc5322ab6d1e058c328e19d2ae147c50ceec997
KuankuanYang <yangkuankuanchn@gmail.com>
2016-12-21 02:49:19 +0100

    初始化init-ui.el文件，并迁移相关代码

--- init.el
+++ init.el
@@ -2,45 +2,12 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'init-packages)
+(require 'init-ui)
 
-
-
-;; 关闭声音
-(setq ring-bell-function 'ignore)
-
-;; 全屏开启
-(setq initial-frame-alist (quote ((fullscreen . maximized))))
-
-
-
-;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
-(tool-bar-mode -1)
-
-;; 关闭文件滑动控件
-(scroll-bar-mode -1)
-
-;; 显示行号
-(global-linum-mode 1)
-
-;; 更改光标的样式（不能生效，解决方案见第二集）
-;;(setq-default cursor-type 'bar)
-
-;; 关闭启动帮助画面
-(setq inhibit-splash-screen 1)
-
-;; 更改显示字体大小 16pt
-;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
-;;(set-face-attribute 'default nil :height 160)
 
 ;; 快速打开配置文件
 (defun open-init-file()
---
+++ lisp/init-ui.el
@@ -0,0 +1,28 @@
+;; 所有与 ui 相关的配置内容都放在该文件内
+
+;; 关闭声音
+(setq ring-bell-function 'ignore)
+
+;; 全屏开启
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
+(tool-bar-mode -1)
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;; 显示行号
+(global-linum-mode 1)
+
+;; 更改光标的样式
+;;(setq-default cursor-type 'bar)
+
+;; 关闭启动帮助画面
+(setq inhibit-splash-screen 1)
+
+;; 更改显示字体大小 16pt
+;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+;;(set-face-attribute 'default nil :height 160)
+
+(provide 'init-ui)
